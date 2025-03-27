29666e218e91a7745c0d6340d943003f1db0d7ff
qntmblck <jcomeaux@ug.uchile.cl>
2025-03-27 20:10:00 -0300

    main

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Scar Transportes') }}</title>
+        <title inertia>{{ config('app.name', 'SGX') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('img/sgxico.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
